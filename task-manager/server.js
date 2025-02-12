671e7ac673a0a895a38aab1c22beeba1b2259fda
Vaishnavi_Katkar <vaishnavi.katkat33@gmail.com>
2025-02-12 12:16:45 +0530

    changes

--- task-manager/server.js
+++ task-manager/server.js
@@ -7,8 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 const TaskSchema = new mongoose.Schema({
